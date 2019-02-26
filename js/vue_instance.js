bcb974f4eae49854af07e343c73d8525acd07d72
Užák Martin <martin.uzak@alpiq.com>
2019-02-26 09:07:28 +0100

    initial commit

---
+++ js/vue_instance.js
@@ -0,0 +1,46 @@
+/*
+ * vue_instance.js
+ * Copyright (C) 2019 Martin Užák martin.uzak@gmail.com
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+'use strict';
+
+
+var  vm = new Vue({
+   el: '#vue_det',
+   data: {
+      firstname : "Ria",
+      lastname  : "Singh",
+      address    : "Mumbai"
+   },
+   methods: {
+      mydetails : function() {
+         return "I am "+this.firstname +" "+ this.lastname;
+      }
+   }
+});
+
+console.log(vm.$data);
+
+
+// must use function when in Vue.extend()
+var Component = Vue.extend({
+  data: function () {
+     return {"name" : "martin", "age" : 15}
+  }
+});
+var myComponentInstance = new Component();
+console.log(myComponentInstance.$data);
+console.log(myComponentInstance.age);
+
+var Comp = Vue.extend({
+   props: ['msg'],
+   template: '<div>{{ msg }}</div>'
+})
+var vm = new Comp({
+   propsData: {
+      msg: 'hello'
+   }
+})
